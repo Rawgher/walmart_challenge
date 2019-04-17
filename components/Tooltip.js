bb9653d89969eb0d2fcd1771ce67f0afddff8ced
Roger <rdpender3@gmail.com>
2019-04-17 07:03:09 -0700

    fixed tooltip

--- components/Tooltip.js
+++ components/Tooltip.js
@@ -4,11 +4,13 @@ import { StyleSheet, Text, View } from "react-native";
 export default class Tooltip extends Component {
   render() {
     return (
-      <View style={styles.tooltipStyle}>
-        <Text>
-          Picking up your order in the store helps cut costs, and we pass the
-          savings on to you!
-        </Text>
+      <View style={[styles.arrowUp, styles.triangle]}>
+        <View style={styles.tooltipStyle}>
+          <Text style={styles.whiteText}>
+            Picking up your order in the store helps cut costs, and we pass the
+            savings on to you!
+          </Text>
+        </View>
       </View>
     );
   }
@@ -16,11 +18,36 @@ export default class Tooltip extends Component {
 
 const styles = StyleSheet.create({
   tooltipStyle: {
-    width: 200,
+    width: 150,
+    height: 100,
     justifyContent: "center",
     borderColor: "black",
     borderWidth: 1,
-    padding: 1,
-    marginTop: 2
+    padding: 10,
+    marginTop: 10,
+    zIndex: 1,
+    fontWeight: "bold",
+    backgroundColor: "black",
+    marginLeft: -70
+  },
+  arrowUp: {
+    borderTopWidth: 0,
+    borderRightWidth: 10,
+    borderBottomWidth: 10,
+    borderLeftWidth: 10,
+    borderTopColor: "transparent",
+    borderRightColor: "transparent",
+    borderBottomColor: "black",
+    borderLeftColor: "transparent",
+    marginLeft: 35
+  },
+  triangle: {
+    width: 0,
+    height: 0,
+    backgroundColor: "transparent",
+    borderStyle: "solid"
+  },
+  whiteText: {
+    color: "white"
   }
 });
